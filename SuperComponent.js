47035ec68e9a3401a747e32597189af83468b203
JaumeJulia-Polarier <139464118+JaumeJulia-Polarier@users.noreply.github.com>
2025-04-30 10:41:45 +0200

    Cambio 4

--- SuperComponent.js
+++ SuperComponent.js
@@ -7,6 +7,7 @@ const SuperComponent = () => {
             {/* Your component content goes here */}
             {/* Your component content goes here */}
             {/* Your component content goes here */}
+            {/* Your component content goes here */}
         </div>
     );
 };
